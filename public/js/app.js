8bb819f127cfe284655f2d6275e3dc45aa9d3cad
maitabe <martushb@gmail.com>
2016-12-27 18:18:07 +0200

    setting up controllers and service

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,26 @@
 var app = angular.module('rereddit', ['ui.router']);
+
+app.config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: '/templates/home.html',
+        controller: 'MainCtrl',
+        resolve: {
+	    postPromise: ['posts', function (posts){
+	      return posts.getAll();
+	    }]
+	  }
+
+      })
+      .state('post', {
+        url: '/posts/:id',
+        templateUrl: '/templates/posts.html',
+        controller: 'PostsCtrl'
+      });
+
+      $urlRouterProvider.otherwise('home');
+}]);
--- public/js/services/posts.js
+++ public/js/services/posts.js
@@ -1,11 +1,39 @@
 app.factory('posts', ['$http', function($http) {
-  var postService = {
-    posts: [],
+    var postService = {
+      posts: [],
 
-    getAll: function() {
-      // blank for now
-    }
-  };
+      getAll: function() {
+        return $http.get('/posts').then(function(data) {
 
-  return postService;
-}]);
+          angular.copy(data.data, postService.posts);
+        });
+      },
+
+      // for GETting one post and it's comments
+      get: function(id) {
+
+      },
+
+      // for POSTing one new post
+      create: function(post) {
+
+      },
+
+      // for increasing the upvotes to one post
+      upvote: function(post) {
+
+      },
+
+      // for adding a comment to one post
+      addComment: function(id, comment) {
+
+      },
+
+      // for upvoting a comment on a specific post
+      upvoteComment: function(post, comment) {
+
+      }
+    };
+
+    return postService;
+  }]);
